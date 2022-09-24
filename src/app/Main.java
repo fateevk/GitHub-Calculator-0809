99de6c79446f90325b26f8f797b36edef6b851c7
fateevk <fateevk@gmail.com>
2022-09-24 13:10:46 +0300

    Declaring variables outside of main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,19 +1,22 @@
 package app;
 
 public class Main {
-
+    static int a = 8;
+    static int b = 2;
+    static int c;
+    static int d;
+    static int e;
+    static int f;
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int e = subtraction(a, b);
+        e = subtraction(a, b);
         System.out.println(e);
-        int f = division(a, b);
+        f = division(a, b);
         System.out.println(f);
     }
 
@@ -24,9 +27,11 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+
     private static int subtraction(int a, int b) {
         return a - b;
     }
+
     private static int division(int a, int b) {
         return a / b;
     }
